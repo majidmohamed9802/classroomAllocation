33681599a69e0b2222517ea94a72598898a820dc
admin <admin@admins-MacBook-Pro.local>
2024-02-13 14:45:30 +0000

    first commit

---
+++ src/main/java/com/classroom/ClassroomAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.classroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomAllocationApplication.class, args);
+	}
+
+}
